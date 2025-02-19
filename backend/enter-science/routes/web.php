0a49d6f55eecb0acbf302ddabeb11fb3a83ae050
Thiago São Pedro <142845683+ThiagoSaoPedro@users.noreply.github.com>
2025-02-19 20:03:33 -0300

    remove archives

--- backend/enter-science/routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-use App\Http\Controllers\EventController;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::post('/event/store', [EventController::class, 'store'])->name('event.store');
