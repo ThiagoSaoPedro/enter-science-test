2a6aeb93e936437a08c9f8d64c8535ea10e6b249
Thiago São Pedro <142845683+ThiagoSaoPedro@users.noreply.github.com>
2025-02-19 22:36:50 -0300

    add comments

--- backend/app/Http/Middleware/Cors.php
+++ backend/app/Http/Middleware/Cors.php
@@ -9,8 +9,8 @@ class Cors
     public function handle(Request $request, Closure $next)
     {
         return $next($request)
-            ->header('Access-Control-Allow-Origin', 'http://localhost:5173')
-            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-            ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+            ->header('Access-Control-Allow-Origin', 'http://localhost:5173') //Allows requests to be made only from the origin
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS') //Defines which HTTP methods are allowed
+            ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization'); //Allows tokens to be sent and json's to be received
     }
 }
